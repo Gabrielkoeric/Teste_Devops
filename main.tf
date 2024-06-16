31ce8aeccd70a19d2f04c40f611da1c146fc8386
Gabrielkoeric <gabriel.koerich@unidavi.edu.br>
2024-06-16 19:38:03 -0300

    gerar VPC completo e ec2

--- main.tf
+++ main.tf
@@ -1,24 +1,104 @@
-terraform{
-    required_providers{
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.27"
-        }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
+  }
 
-    required_version = ">= 0.14.9"
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-    profile = "default"
-    region = "us-west-2"
+  profile = "default"
+  region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server"{
-    ami = "ami-0cf2b4e024cdb6960"
-    instance_type = "t2.micro"
-    key_name = "devops"
-    tags = {
-        Name = "app"
-    }
+#Gerar VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "routetable" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id = aws_subnet.subnet.id
+  route_table_id = aws_route_table.routetable.id
+}
+
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "liberar todo o trafego"
+  }
+}
+
+#Gerar EC2 
+resource "aws_instance" "app_server" {
+  ami = "ami-0cf2b4e024cdb6960"
+  instance_type = "t2.micro"
+  key_name = "devops"
+  subnet_id = aws_subnet.subnet.id
+  associate_public_ip_address = true
+  security_groups = [aws_security_group.sg.id]
+  tags = {
+    Name = "app"
+  }
+
+  depends_on = [
+    aws_security_group.sg,
+    aws_internet_gateway.gw,
+    aws_route_table_association.rta
+  ]
+
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.subnet.id
+}
+
+output "instance_id" {
+  value = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.app_server.public_ip
 }
