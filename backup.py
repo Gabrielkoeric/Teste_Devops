2574fbdf2fe8d0e4e870814732f27af9fc15c660
Gabrielkoeric <gabriel.koerich@unidavi.edu.br>
2024-06-18 21:04:46 -0300

    ajuste script de backup

--- backup.py
+++ backup.py
@@ -1,8 +1,9 @@
 import subprocess
 import datetime
+import boto3
+import os
 
-def backup_postgresql_db(db_name, db_user, db_password, db_host, db_port, output_dir):
-    
+def backup_postgresql_db(db_name, db_user, db_password, db_host, db_port, output_dir, s3_bucket, s3_key_prefix):
     backup_file = f"{output_dir}/{db_name}_backup_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.sql"
 
     command = [
@@ -10,7 +11,7 @@ def backup_postgresql_db(db_name, db_user, db_password, db_host, db_port, output
         '-h', db_host,
         '-p', str(db_port),
         '-U', db_user,
-        '-F', 'c', 
+        '-F', 'c',
         '-f', backup_file,
         db_name
     ]
@@ -20,8 +21,18 @@ def backup_postgresql_db(db_name, db_user, db_password, db_host, db_port, output
     try:
         result = subprocess.run(command, env=env, check=True, capture_output=True, text=True)
         print(f"Backup do banco de dados '{db_name}' concluído com sucesso. Arquivo: {backup_file}")
+        
+        s3 = boto3.client('s3')
+        s3_key = f"{s3_key_prefix}/{os.path.basename(backup_file)}"
+        s3.upload_file(backup_file, s3_bucket, s3_key)
+        print(f"Arquivo de backup '{backup_file}' enviado para o bucket S3 '{s3_bucket}' com a chave '{s3_key}'")
+        
+        os.remove(backup_file)
+        
     except subprocess.CalledProcessError as e:
         print(f"Erro ao realizar o backup: {e.stderr}")
+    except Exception as e:
+        print(f"Erro ao enviar o arquivo para o S3: {str(e)}")
 
 if __name__ == "__main__":
     db_name = 'testdb'
@@ -30,5 +41,9 @@ if __name__ == "__main__":
     db_host = 'localhost'
     db_port = 5432
     output_dir = '/home'
+    
+    # Configurações do S3
+    s3_bucket = 'backupbucketgabriel'
+    s3_key_prefix = 'backups'
 
-    backup_postgresql_db(db_name, db_user, db_password, db_host, db_port, output_dir)
+    backup_postgresql_db(db_name, db_user, db_password, db_host, db_port, output_dir, s3_bucket, s3_key_prefix)
